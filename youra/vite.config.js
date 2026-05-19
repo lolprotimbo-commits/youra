330966dc05acdbd38f4c3b65c571af1e025ed3ff
lolprotimbo-commits <lolprotimbo@gmail.com>
2026-05-19 06:32:42 +0200

    Delete youra directory

--- youra/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // If deploying to GitHub Pages under a repo subfolder, set base:
-  // base: '/youra/',
-  base: '/',
-})
